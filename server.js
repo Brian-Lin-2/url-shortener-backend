cbef67d94c9b4f8fec9f8031789128a30d715168
Brian <linbrian24@gmail.com>
2023-08-27 19:12:22 -0400

    Add database retrieval and insertion (user data)

--- server.js
+++ server.js
@@ -14,6 +14,38 @@ const db = mysql.createConnection({
   database: "user-database",
 })
 
+app.post('/register', (req, res) => {
+  const sql = "INSERT INTO `user-database`.`login` (`email`, `password`) VALUES (?, ?)";
+
+  // req is in array format from user form.
+  const email = req.body[0];
+  const password = req.body[1];
+
+  db.query(sql, [email, password], (error, result) => {
+    if (result) {
+      res.send(result);
+    } else {
+      res.send({ message: "Email Is Taken" });
+    }
+  });
+})
+
+app.post('/login', (req, res) => {
+  const sql = "SELECT * FROM `user-database`.`login` WHERE `email` = ? AND `password` = ?";
+
+  // req is in array format from user form.
+  const email = req.body[0];
+  const password = req.body[1];
+
+  db.query(sql, [email, password], (error, result) => {
+    if (result.length > 0) {
+      res.send(result);
+    } else {
+      res.send({ message: "Invalid Email/Password" });
+    }
+  });
+})
+
 // Ensures the server is loaded.
 app.listen(3306, () => {
   console.log("running");
